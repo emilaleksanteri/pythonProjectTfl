a435d32102cc8f5ea31fe128bbcbc9d14c884dc1
Emil Lystimaki <emilaleksanteri@gmail.com>
2023-04-15 15:33:39 +0100

    comments

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from stopTimes import StopTimes
 
+'''
+ data schema from objects:
+[{
+    "lineName": string
+    "stationName": string
+    "platFormName": string
+    "direction": string
+    "goingTowards": string
+    "finalDestination": string
+    "timeToStationMins": int
+}]
+'''
+
 
 def main():
     # objects for all stops:
 
+    # Henriques Street (Stop P)
     henriquesStreet = StopTimes("490009276E")
+
+    # New Road (Stop Q)
     newRoad = StopTimes("490010255E")
+
+    # Aldgate East Station (Stop E)
     aldgateEastStationBus = StopTimes("490000004E")
+
+    # Altab Ali Park (Stop D)
     altabAliPark = StopTimes("490006827W")
+
+    # The underground
     aldgateEastUnderground = StopTimes("940GZZLUADE")
