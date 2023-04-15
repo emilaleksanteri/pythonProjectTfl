4987f32b736cfed4003d40f0aafc701cbf72866b
Emil Lystimaki <emilaleksanteri@gmail.com>
2023-04-15 15:30:46 +0100

    all stops as objects for use

---
+++ main.py
@@ -0,0 +1,11 @@
+from stopTimes import StopTimes
+
+
+def main():
+    # objects for all stops:
+
+    henriquesStreet = StopTimes("490009276E")
+    newRoad = StopTimes("490010255E")
+    aldgateEastStationBus = StopTimes("490000004E")
+    altabAliPark = StopTimes("490006827W")
+    aldgateEastUnderground = StopTimes("940GZZLUADE")
