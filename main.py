3bef8dddcb7ca96c15b56a5031f83fe64c1007a0
Emil Lystimaki <emilaleksanteri@gmail.com>
2023-04-16 22:45:54 +0100

    sandbox

--- main.py
+++ main.py
@@ -19,15 +19,23 @@ def main():
 
     # Henriques Street (Stop P)
     henriquesStreet = StopTimes("490009276E")
+    print(henriquesStreet.getStationData())
 
     # New Road (Stop Q)
     newRoad = StopTimes("490010255E")
+    print(newRoad.getStationData())
 
     # Aldgate East Station (Stop E)
     aldgateEastStationBus = StopTimes("490000004E")
+    print(aldgateEastStationBus.getStationData())
 
     # Altab Ali Park (Stop D)
     altabAliPark = StopTimes("490006827W")
+    print(altabAliPark.getStationData())
 
     # The underground
     aldgateEastUnderground = StopTimes("940GZZLUADE")
+    print(aldgateEastUnderground.getStationData())
+
+
+main()
